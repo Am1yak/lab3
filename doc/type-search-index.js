8dd75d6f2e7129a6c55950a5161c9e74dce1068e
Artemiy Moroz <artemiy.moroz@gmail.com>
2024-10-22 18:49:20 +0300

    Added Javadoc files

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"ColorText"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"ShapeText"},{"p":"<Unnamed>","l":"SimpleText"},{"p":"<Unnamed>","l":"TextEditor"}];updateSearchResults();
